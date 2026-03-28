021805efbb9e9f0734869f68baac361c8f24a618
Haarshitha-Potluri <166300870+Haarshitha-Potluri@users.noreply.github.com>
2026-03-28 10:48:54 +0000

    Committing file

---
+++ Backend/src/main/java/com/example/SmartCampusApplication/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.SmartCampusApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/complaints/**").permitAll()
+                .anyRequest().permitAll()
+            )
+            .cors(cors -> {});
+
+        return http.build();
+    }
+}
